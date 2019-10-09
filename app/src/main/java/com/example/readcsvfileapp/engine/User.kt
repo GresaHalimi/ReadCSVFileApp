d1d7023707cb204c4c28d299fa877351e17db59f
Gresa Halimi <ghalimi@mobiquityinc.com>
2019-10-09 14:18:21 +0200

    read csv file

---
+++ app/src/main/java/com/example/readcsvfileapp/engine/User.kt
@@ -0,0 +1,12 @@
+package com.example.readcsvfileapp.engine
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "user")
+data class User(@field:PrimaryKey(autoGenerate = true) var id: Int,
+                var firstname: String,
+                var surname: String,
+                var issueCount: Int,
+                var dateOfBirth: Date?)
